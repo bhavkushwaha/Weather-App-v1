e82814c3f11edd5f4bc959506b00302b72138b97
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-07-04 23:50:21 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="justify-center items-center">
+      <h1 className="text-3xl font-bold underline text-center">Weather App</h1>
+      <p>Know about the weather update of your city!</p>
+    </div>
+  );
+};
+
+export default Header;
