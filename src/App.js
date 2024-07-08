aa911997bbff2f86b8a50fe7a4b98f0423c8a8e6
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-07-08 11:06:32 +0530

    initialized project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
