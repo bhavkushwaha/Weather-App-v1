269e48addb1b8d8a569ec3fdb7be4fc686e3bacc
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-07-09 13:10:34 +0530

    webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,9 @@ require("dotenv").config({ path: "./.env" });
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env),
+    new Dotenv({
+      systemvars: true,
     }),
   ],
 };
