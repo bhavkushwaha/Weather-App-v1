3bd591d4a1486d877ea7775e85a478e9131259e1
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-07-09 11:54:48 +0530

    test3

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon");
 
-const API_KEY = `${process.env.REACT_APP_WEATHER_API_KEY}`;
-// console.log(API_KEY);
+const API_KEY = process.env.REACT_APP_VERCEL_API_KEY;
+console.log(API_KEY);
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
 const getWeatherData = (infoType, searchParams) => {
